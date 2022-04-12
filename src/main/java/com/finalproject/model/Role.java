ef21a3ead336fe351eaa3018eed5414b5761276f
xiaohuanlin <xiaohuanlin1993@gmail.com>
2022-04-12 17:24:31 -0400

    first init

---
+++ src/main/java/com/finalproject/model/Role.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.finalproject.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+
+/**
+ *
+ * @author Administrator
+ */
+@Entity
+public class Role {
+    @Id
+    private Integer id;
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
